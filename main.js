d56d7ec2e33983671e41fa843214a9fb26f47f3e
SarvarbekNematov <meoff1011@gmail.com>
2024-03-19 09:49:49 +0500

    vazifa

---
+++ main.js
@@ -0,0 +1,11 @@
+// let arr = ['olcha', 'anor', 'shaftoli', 'olma'];
+
+// let a = prompt("qiymat kiriting");
+// let newArr = arr.filter((value)=> {
+//     if(a != value){
+//         return value;
+//     }
+// })
+
+// console.log(newArr);
+
