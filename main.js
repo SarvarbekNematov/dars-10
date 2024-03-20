f4975136e863f54e945874ce6b06c99052977f09
SarvarbekNematov <meoff1011@gmail.com>
2024-03-21 00:41:09 +0500

    vazifa tugadi

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+// let arr = ['1', '2', '3', '4', 'a', '5', 'b', '6'];
+// let newArr = [];
+// const x = (a) => {
+//     for(let i = 0; i < a.length; i++){
+//         let b = Number(a[i]);
+//         if(b){
+//             newArr.push(b);
+//         }
+//     }
+//     return newArr
+// }
+
+// console.log(x(arr));
+
+    // MASALA
+
+// let arr = [1,32,4,12,421,142,421,214];
+
+// let max = arr[0];
+// for(let i of arr){
+//     if(max < i){
+//         max = i;
+//     }
+// }
+// console.log(max);
+
+    // MASALA
+
 // let arr = ['olcha', 'anor', 'shaftoli', 'olma'];
 
 // let a = prompt("qiymat kiriting");
@@ -8,4 +36,108 @@
 // })
 
 // console.log(newArr);
+
+    // MASALA
+
+// let str = "32hu3h2h3y";
+// let arr = [];
+// const x = (a) => {
+//     for(let i = 0; i < a.length;i++){
+//         let b = Number(a[i]);
+//         if(b){
+//             arr.push(b)
+//         }
+//     }
+//     return arr
+// }
+
+// console.log(x(str));
+
+    // MASALA
+
+// let arr = [12,332,1312,321,4,31];
+// let min = arr[0]
+// let newArr = []
+// const x = (a) => {
+//     for(let i = 0; i < a.length;i++){
+//         if(min > a[i]){
+//         min = a[i];
+//         }
+//     }
+//     newArr.push(min);
+//     for(let i = 0; i < a.length;i++){
+//         if(min < a[i]){
+//         min = a[i];
+//         }
+//     }
+//     newArr.push(min);
+//     return newArr
+// }
+
+// console.log(x(arr));
+
+    // MASALA
+
+// let arr = [123,1,0,321,12,3321,32,12];
+// let newArr = arr.filter(i => i > 2)
+
+
+// console.log(newArr);
+
+    // MASALA
+
+// let arr = [123,312,23,12,21,343,431];
+// let juftSonla = 0;
+// let toqSonla = 0;
+// let umumiy = []
+// const x = (a) => {
+//     for(let i = 0; i < a.length;i++){
+//         if(a[i] % 2 == 0){
+//             juftSonla++
+//         }
+//         else{
+//             toqSonla++
+//         }
+//     }   
+//     umumiy.push(juftSonla + "ta juft sonla");
+//     umumiy.push(toqSonla  + "ta toq sonla");
+//     return umumiy
+// }
+
+// console.log(x(arr));
+
+    // MASALA
+
+    // function yigindisi(...sonlar) {
+    //     return sonlar.reduce((a, b) => a + b, 0);
+    //   }
+    //   let natija = yigindisi(2, 1, 32, 312);
+    //   console.log(natija); 
+      
+    // MASALA
+
+    // let obj = {
+    //     name: "ali",
+    //     age: 18,
+    //     city: "tawkent",
+    // };
+
+    // const x = (a) => {
+    //     return Object.values(a)
+    // }
+    // const y = (b) => {
+    //     return Object.keys(b)
+    // }
+
+    // let valuesArr = x(obj);
+    // let keysArr = y(obj);
+    
+    // console.log(keysArr);
+    // console.log(valuesArr);
+
+        
+    // <<# Assalomu alaykum oxirgisini tuwunmadm wuncun iwlamadm kecirasz >>
+
+
+
 
